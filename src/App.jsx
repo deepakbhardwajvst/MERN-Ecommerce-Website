c51978c9831d1fd67642a0a15ab4bbd34b5b4826
deepakbhardwajcms <deepak.bhardwaj@clingmultisolutions.org>
2024-08-30 16:14:20 +0530

    depandancy install

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+function App() {
+
+
+  return (
+    <>
+      <div className='bg-red-700 text-black w-96 h-36 text-9xl'>gkrligsd
+       </div>
+    </>
+  )
+}
+
+export default App
